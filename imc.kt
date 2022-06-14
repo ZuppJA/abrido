47baba07353dad5bf5bcdcb5f3da9528ff42d89a
ZuppJA <89170769+ZuppJA@users.noreply.github.com>
2022-06-14 19:16:09 -0400

    abrido

---
+++ imc.kt
@@ -0,0 +1,17 @@
+import java.util.Scanner
+fun imc (peso:Float, altura:Float): Float {
+    return peso/(altura*altura)
+}
+fun main(){
+    val teclado=Scanner(System.´in´)
+    print("Digite o peso\t");
+    var peso:Float=teclado.nextFloat()
+    print("Digite a altura:\t")
+    var altura:Float=teclado.nextFloat()
+    if (imc(peso,altura)<18)
+    print("Sobpeso")
+    else if(imc(peso,altura)>18 && imc(peso,altura)<25)
+    print("ok")
+    else
+    print("Sobrepeso");
+}
---
+++ soma.kt
@@ -0,0 +1,15 @@
+fun soma(x:Int, y:Int): Int {
+    return x+y
+}
+
+fun sub(x:Int, y:Int): Int {
+    return x-y   
+}
+
+fun main( ) {
+    println("Soma de dois números");
+    println(soma(4,3))
+    println("Subtração entre dois números");
+    println(sub(4,3))
+
+}
